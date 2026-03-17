cb0022d881d0a8e8b2c9cd7d9538520301f0eaef
deligeo <georgedelna10@gmail.com>
2026-03-16 22:54:18 -0400

    Added logout function to AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,4 +32,12 @@ class AuthController extends Controller
         return back()->withErrors(['email' => 'Invalid credentials']);
     }
 
+    // Logout
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login');
+    }
 }
